d40d6557f64ba5f561aa5ab9e1f8e75a9aa62d29
Ruimtevaart <granata0520@gmail.com>
2020-10-08 21:42:10 +0300

    Added writing/reading to json file functionality

--- Priceredacted/UI/SearchData.cs
+++ Priceredacted/UI/SearchData.cs
@@ -9,13 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
 using System.Data.SqlClient;
-
+using Newtonsoft.Json;
+using Priceredacted.Search;
 
 namespace Priceredacted
 {
     public partial class SearchData : Form
     {
-        
+        string path = @"C:\Universitetas\Trecias_semestras\Programu_sistemu_inzinerija\Testing\Priceredacted\Priceredacted\DB\Products.json";
 
         public SearchData()
         {
@@ -35,12 +36,55 @@ namespace Priceredacted
 
         private void AddData_button_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                List<Product> Products = JsonConvert.DeserializeObject<List<Product>>(System.IO.File.ReadAllText(path));
+                if (Products == null)
+                {
+                    Products = new List<Product>();
+                }
+                Products.Add(new Product()
+                {
+                    Shop = ShopList.Text.Trim(),
+                    Group = ItemGroup.Text.Trim(),
+                    Name = ProductName.Text.Trim(),
+                    PriceUnit = PriceUnit.Text.Trim(),
+                    Price = Price.Text.Trim()
+                });
+                string json = JsonConvert.SerializeObject(Products.ToArray());
+                System.IO.File.WriteAllText(path, json);
+                MessageBox.Show("Data saved");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
         }
 
         private void SearchButton_Click(object sender, EventArgs e)
         {
-            
+            List<Product> UnfilteredProducts = JsonConvert.DeserializeObject<List<Product>>(System.IO.File.ReadAllText(path));
+            string query = SearchBox.Text.Trim().ToLower();
+            List<Product> FilteredProducts = new List<Product>();
+            if (query == "")
+            {
+                FilteredProducts = UnfilteredProducts;
+            }
+            else
+            {
+                foreach (Product pr in UnfilteredProducts)
+                {   
+                    if (pr.Shop.ToLower() == query || pr.Group.ToLower() == query || pr.Name.ToLower() == query || pr.PriceUnit.ToLower() == query || pr.Price.ToLower() == query)
+                    {
+                        FilteredProducts.Add(pr);
+                    }
+                }
+            }
+            if (FilteredProducts != null)
+            {
+                SearchResults.DataSource = FilteredProducts;
+            }
+            else MessageBox.Show("No relevant data found");
         }
 
         private void PriceRedacted_Load(object sender, EventArgs e)
