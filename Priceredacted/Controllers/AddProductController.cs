15086736b79f63f60d31fd7f7ad9eddf3926ddac
Mantas-Meow <71716747+Mantas-Meow@users.noreply.github.com>
2020-11-12 23:42:48 +0200

    test

---
+++ Priceredacted/Controllers/AddProductController.cs
@@ -0,0 +1,31 @@
+﻿using Priceredacted.Logics;
+using Priceredacted.Processors;
+using Priceredacted.Properties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using static Priceredacted.Tools.Utils;
+
+namespace Priceredacted.Controllers
+{
+    class AddProductController
+    {
+        private AddProductLogic AddLogic;
+        private MainWindowController Main;
+
+        public AddProductController()
+        {
+            Main = new MainWindowController();
+            AddLogic = new AddProductLogic();
+        }
+
+        public void AddProducts(Shops shop, string group,
+                string name, string priceUnit, string price)
+        {
+            Main.AddData(shop, group, name, priceUnit, price);
+            AddLogic.AddProduct(shop, name, priceUnit, price);
+        }
+
+    }
+}
---
+++ Priceredacted/UI/AddProductWindow.cs
@@ -0,0 +1,28 @@
+﻿using Priceredacted.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using static Priceredacted.Tools.Utils;
+
+namespace Priceredacted.UI
+{
+    public partial class AddProductWindow : Form
+    {
+        AddProductController AddController;
+
+        public AddProductWindow()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddController.AddProducts((Shops)Enum.Parse(typeof(Shops), ShopList.Text), ItemGroup.Text,        //((Shops)Enum.Parse(typeof(Shops), ShopList.Text))
+                    ProductName.Text.Trim(), PriceUnit.Text, Price.Text.Trim());
+        }
+
+    }
+}
