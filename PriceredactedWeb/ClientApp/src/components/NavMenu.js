7c15bd79b5dd9d4fac1750a51474cd6e8b8b93fb
Bičiulaitis Grantas <grantas.biciulaitis@mif.stud.vu.lt>
2020-11-27 11:13:39 +0200

    Small update to UI

--- PriceredactedWeb/ClientApp/src/components/NavMenu.js
+++ PriceredactedWeb/ClientApp/src/components/NavMenu.js
@@ -34,10 +34,10 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Scan image</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Search Items</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
