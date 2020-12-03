a9c9ef2a57af9d5c45d63859591fe9f20b938044
Benas Kopūstas <benas.kopustas@mif.stud.vu.lt>
2020-12-03 23:45:37 +0200

    Added app.config and changed paths in Utils to use paths in app.config

--- Priceredacted/Tools/Utils.cs
+++ Priceredacted/Tools/Utils.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Reflection.Metadata;
 using System.Text;
+using System.Configuration;
 
 namespace Priceredacted.Tools
 {
     static public class Utils
     {
+        /*public static string ProductsPath = System.IO.Directory.GetParent(Environment.CurrentDirectory).
+            Parent.Parent.FullName + "\\DB\\Products.json";*/
         public static string ProductsPath = System.IO.Directory.GetParent(Environment.CurrentDirectory).
-            Parent.Parent.FullName + "\\DB\\Products.json";
+             Parent.Parent.FullName + ConfigurationManager.AppSettings["ProductsPath"];
 
         public static string UserDataPath = System.IO.Directory.GetParent(Environment.CurrentDirectory).
-            Parent.Parent.FullName + "\\DB\\UserData.json";
+             Parent.Parent.FullName + ConfigurationManager.AppSettings["UserDataPath"];
+        //public static string UserDataPath = ConfigurationManager.AppSettings["UserDataPath"];
 
+        /*public static string ReceiptsPath = System.IO.Directory.GetParent(Environment.CurrentDirectory).
+            Parent.Parent.FullName + "\\DB\\Receipts.json";*/
         public static string ReceiptsPath = System.IO.Directory.GetParent(Environment.CurrentDirectory).
-            Parent.Parent.FullName + "\\DB\\Receipts.json";
+             Parent.Parent.FullName + ConfigurationManager.AppSettings["ReceiptsPath"];
         public enum Shops {Default, Maxima, Lidl, Iki, Rimi, Norfa};
     }
 }
