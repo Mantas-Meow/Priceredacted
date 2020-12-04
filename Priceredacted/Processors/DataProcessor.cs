7bd72f922500940128030320060367607cf54635
Deimantas Eidukevicius <Diamond322222@gmail.com>
2020-12-04 16:36:24 +0200

    lock usage in DataProcessor

--- Priceredacted/Processors/DataProcessor.cs
+++ Priceredacted/Processors/DataProcessor.cs
@@ -7,14 +7,20 @@ namespace Priceredacted.Processors
 {
     static class DataProcessor
     {
+        private static object _lock = new object();
         public static IEnumerable<T> LoadJson<T>(string path)
         {
             return JsonConvert.DeserializeObject<IEnumerable<T>>(System.IO.File.ReadAllText(path));
         }
+           
         public static void SaveJson<T>(IEnumerable<T> objects, string path)
         {
-            string json = JsonConvert.SerializeObject(objects, Formatting.Indented);
-            System.IO.File.WriteAllText(path, json);
+
+            lock (_lock)
+            {
+                string json = JsonConvert.SerializeObject(objects, Formatting.Indented);
+                System.IO.File.WriteAllText(path, json);
+            }
         }
     }
 }
