975073abb931738947ec94528482e884cef3089e
Bičiulaitis Grantas <grantas.biciulaitis@mif.stud.vu.lt>
2020-12-10 21:55:05 +0200

    Small update to frontend

--- PriceredactedWeb/ClientApp/src/components/FetchData.js
+++ PriceredactedWeb/ClientApp/src/components/FetchData.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import './Buttons.css'
+import './DropDowns.css'
 
 export class FetchData extends Component {
   static displayName = FetchData.name;
@@ -46,18 +49,74 @@ export class FetchData extends Component {
         let contents = this.state.loading ? <p><em>Loading...</em></p> : FetchData.renderForecastsTable(this.state.forecasts);
 
       return (
-        <main>
+          <main>
              <div>
                 <span className="headline-text">Search</span>
-                <p>Search for a product</p>
+                  <p>Search for a product</p>
+              </div>
+              <div>
+                  <Grid container direction="row" spacing={4}>
+                      <Grid item>
+                          <button className="basicButton" /*onClick={() => setUpload(true)}*/>Filter</button>
+                      </Grid>
+                      <Grid item>
+                          <label for="Shop">Shop:</label>
+                          <select name="Shop" className="basicDropDown" id="Shop">
+                              <option value="Iki">Iki</option>
+                              <option value="Lidl">Lidl</option>
+                              <option value="Maxima">Maxima</option>
+                              <option value="Norfa">Norfa</option>
+                              <option value="Rimi">Rimi</option>
+                          </select>
+                      </Grid>
+                      <Grid item>
+                          <label for="ItemGroup">Item group:</label>
+                          <select name="ItemGroup" className="basicDropDown" id="ItemGroup">
+                              <option value="Fruit">Fruit</option>
+                              <option value="Vegetable">Vegetable</option>
+                              <option value="Dairy">Dairy</option>
+                              <option value="Meat">Meat</option>
+                              <option value="Baked goods">Baked goods</option>
+                              <option value="Electronics">Electronics</option>
+                              <option value="Sweets">Sweets</option>
+                              <option value="Alc. Drinks">Alc. Drinks</option>
+                              <option value="Soda">Soda</option>
+                              <option value="Juice">Juice</option>
+                              <option value="Snacks">Snacks</option>
+                              <option value="Pasta">Pasta</option>
+                              <option value="Icecream">Icecream</option>
+                              <option value="Frozen goods">Frozen goods</option>
+                              <option value="Bathroom goods">Bathroom goods</option>
+                              <option value="Cleaning">Cleaning</option>
+                              <option value="Pets">Pets</option>
+                          </select>
+                      </Grid>
+                      <Grid item>
+                          <label for="Product">Product:</label>
+                          <input type="text" id="Product" name="Product"></input>
+                      </Grid>
+                      <Grid item>
+                          <label for="PriceUnit">Price unit:</label>
+                          <select name="PriceUnit" className="basicDropDown" id="PriceUnit">
+                              <option value="Eur/Unit">Eur/Unit</option>
+                              <option value="Eur/kg">Eur/kg</option>
+                          </select>
+                      </Grid>
+                      <Grid item>
+                          <label for="Price">Price:</label>
+                          <input type="text" id="Price" name="Price"></input>
+                      </Grid>
+                  </Grid>
+              </div>
+              <div>
                 {contents}
-            </div>
+             </div>
         </main>
     );
   }
 
   async populateWeatherData() {
-      const response = await fetch('https://localhost:5001/api/Products');
+      const response = await fetch('https://localhost:44310/api/Products');
     const data = await response.json();
     this.setState({ forecasts: data, loading: false });
   }
